dc3d34639f4cc1172ef6682d71819d4bf511412c
Abhishek Gupta <abhishek.gupta_cs13@gla.ac.in>
2017-08-31 08:48:26 +0530

    added comments

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import fs from 'fs'; // for file system
-import bodyParser from 'body-parser';
-import connect from './config/config';
-import find from './routes/find';
-import del from './routes/delete';
-import insert from './routes/insert';
-import update from './routes/update';
-import http from 'http';
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const fs = require('fs'); // for file system
+const bodyParser = require('body-parser');
+const connect = require('./config/config');
+const find = require('./routes/find');
+const del = require('./routes/delete');
+const insert = require('./routes/insert');
+const update = require('./routes/update');
+const index = require('./routes/index');
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json());  // parse body in json format
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(morgan('dev'));
 app.use(morgan('common', {stream: fs.createWriteStream('./logs/read-in-employee.log',{flags: 'a'})}));
+app.use('/',index);			// add routes for index.js
+app.use('/find', find);	// add routes for find.js
+app.use('/delete', del);	// add routes for delete.js
+app.use('/update', update);	// add routes for update.js
+app.use('/insert', insert);	// add routes for insert.js
 
-app.use('/find', find);
-app.use('/delete', del);
-app.use('/update', update);
-app.use('/insert', insert);
-
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-app.listen(port, () => {
+app.listen(3000, () => {				// listening to port 3000
   console.log('Example app listening on port 3000!')
 });
